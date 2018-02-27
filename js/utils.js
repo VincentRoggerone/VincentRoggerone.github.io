5ba802b8d75e2fbacb5189412e668de9405e8ced
VincentRoggerone <centvin83@hotmail.com>
2018-02-27 12:40:39 -0500

    Dernieres modif forme
    Dernier reglage de la form

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,38 @@
 function sidebar(){
-	document.write('<div id="sidebar"> <div id="sidebar-fixed"> <img src="Id_cercle.png" title="my photo" id="my_photo" width="250"/> <h3>Contact</h3> <p> Mcgill Campus <br /> School of information Studies <br /> 3661 Rue Peel <br /> Montréal, QC H3A 1X1 <br /> Canada <br /> firstname.lastname@mcgill.ca <br /> <a href="#">https://vincentroggerone.github.io/</a></p> </div></div>');	
+	document.write(	'<div id="sidebar">'+
+					'<img class="center" src="Id_cercle.png" title="my photo" id="my_photo" width="120px" align="middle"/>'+
+					//'<h3>Contact</h3>'+
+					'<h3>Vincent Roggerone</h3> '+
+					'<p> <ul class="sideb"> <li> School of information Studies  </li> <li> 3661 Rue Peel  </li> <li>  Montréal, QC  </li> <li> H3A 1X1 CANADA  </li> </ul> <ul class="sidea"> <li> firstname.lastname </li> <li> @mcgill.ca  </li></ul> </p>'+
+				   '</div>');	
 		}
 
 
 function banner(){
-	document.write('<div id="banner"> <div id="name"> <h1>Vincent Roggerone</h1> <p>PhD in Acoustic</p> <p> School of Information Studies </p> <p>McGill, Montréal, CANADA</p> </div> <div id="welc"> <h1> Bienvenue </h1> </div> </div>');	
+	document.write('<div id="banner">'+
+					'<div id="name">'+
+						'<h1>Vincent Roggerone</h1> <p>PhD in Acoustic</p> <p> School of Information Studies </p> <p>McGill, Montréal, CANADA</p>'+	
+					'</div>'+
+					'<div id="welc">'+
+						'<h1> Bienvenue </h1>'+
+					'</div>'+
+				   '</div>');	
 		}
 		
 		
 function navbar(){
-	document.write('<div id="nav"> <ul> <li> <a class="active" href="index.html">Home</a></li> <li><a href="research.html">Research</a></li><li><a href="publi.html">Publications</a></li><li><a href="CV.html">CV</a></li><li><a href="friends.html">Collaborators/Friends</a></li></ul></div>');	
+	document.write('<div id="nav">'+
+					'<ul>'+
+						'<li><a class="active" href="index.html">Home</a></li>'+
+						'<li><a href="research.html">Research</a></li>'+
+						'<li><a href="publi.html">Publications</a></li>'+
+						'<li><a href="CV.html">CV</a></li>'+
+						'<li><a href="friends.html">Collaborators/Friends</a></li>'+
+					'</ul></div>');	
 		}
 
 		
 function footbar(){
-	document.write('<div id="footer"><div id="copyright"> &copy; Vincent Roggerone. Thanks to the University Of Colorado Boulder for the tutorial and template. </div></div>');	
+	document.write('<div id="footer"> &copy; Vincent Roggerone. Thanks to the University Of Colorado Boulder for the tutorial and template. </div>');	
 		}
 		
